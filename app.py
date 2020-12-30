bb89738ee5f290c241d8c5e19b5f824651d7410e
Ahmet Talha Köylü <ahmettalha.net@gmail.com>
2020-12-30 10:03:18 +0300

    Required 3rd party modules were included.

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for,session,logging,request
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, PasswordField, validators
+from passlib.handlers.sha2_crypt import sha256_crypt
 
 app = Flask(__name__)
 
