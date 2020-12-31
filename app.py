38b5d4817c350f3ea821d9342ca1094e754a7884
Ahmet Talha Köylü <ahmettalha.net@gmail.com>
2020-12-31 16:15:24 +0300

    Article update and delete added.

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.config["MYSQL_PASSWORD"] = ""
 app.config["MYSQL_DB"] = "tkblog"
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
-# user register form
+# user register, login, article add form
 class RegisterForm(Form):
     name = StringField("Name and Surname", validators=[validators.Length(min= 3, max= 25), validators.DataRequired()])
     username = StringField("Username", validators=[validators.Length(min= 3, max= 35), validators.DataRequired()])
@@ -34,10 +34,10 @@ class LoginForm(Form):
     username = StringField("Username",validators=[validators.DataRequired()])
     password = PasswordField("Password",validators=[validators.DataRequired()])
 
-class AddArticleForm(Form):
+class ArticleForm(Form):
     title = StringField("Title", validators=[validators.Length(min=10, max=255), validators.DataRequired()])
     content = TextAreaField("Content", validators=[validators.Length(min=100), validators.DataRequired()])
-    thumbnail = StringField("Thumbnail")
+    thumbnail = StringField("Thumbnail", default="/static/img/default.png")
 
 # routes
 @app.route("/")
@@ -78,10 +78,24 @@ def dashboard():
     else:
         return render_template("dashboard.html")
     
+# articles
+
+@app.route('/articles')
+def articles():
+    cursor = mysql.connection.cursor()
+    article_query = "Select * From articles ORDER BY id DESC"
+    result = cursor.execute(article_query)
+
+    if result > 0:
+        articles = cursor.fetchall()
+        return render_template("articles.html", articles = articles)
+    else:
+        return render_template("articles.html")
 
 @app.route('/add-article', methods=["GET","POST"])
+@login_required
 def add_article():
-    form = AddArticleForm(request.form)
+    form = ArticleForm(request.form)
 
     if request.method == "POST" and form.validate():
         title = form.title.data
@@ -96,19 +110,6 @@ def add_article():
         flash(message="The article has been successfully added.", category="success")
         return redirect(url_for("dashboard"))
     return render_template("add-article.html", form = form)
-
-@app.route('/articles')
-def articles():
-    cursor = mysql.connection.cursor()
-    article_query = "Select * From articles ORDER BY id DESC"
-    result = cursor.execute(article_query)
-
-    if result > 0:
-        articles = cursor.fetchall()
-        return render_template("articles.html", articles = articles)
-    else:
-        return render_template("articles.html")
-
      
 @app.route('/article/<string:id>')
 def article_detail(id):
@@ -121,6 +122,58 @@ def article_detail(id):
     else:
         return render_template("article-error.html")
 
+@app.route('/delete/<string:id>')
+@login_required
+def article_delete(id):
+    cursor = mysql.connection.cursor()
+    article_query = "Select * From articles Where author = %s and id = %s"
+    result = cursor.execute(article_query,(session["username"], id))
+    if result > 0:
+        delete_query = "Delete From articles where id = %s"
+        cursor.execute(delete_query,(id,))
+        mysql.connection.commit()
+        flash(message="Article successfully deleted", category="success")
+        return redirect(url_for("dashboard"))
+    else:
+        flash(message="There is no such article or you do not have permission to delete it.", category="danger")
+        return redirect(url_for("dashboard"))
+
+@app.route('/edit/<string:id>', methods=["GET","POST"])
+@login_required
+def article_update(id):
+    if request.method == "GET":
+        cursor = mysql.connection.cursor()
+        article_query = "Select * From articles where author = %s and id = %s"
+        result = cursor.execute(article_query, (session["username"], id))
+        if result > 0:
+            article = cursor.fetchone()
+            form = ArticleForm()
+            form.title.data = article["title"]
+            form.content.data = article["content"]
+            form.thumbnail.data = article["post_thumbnail"]
+            return render_template("update-article.html", form = form)
+        else:
+            flash(message="There is no such article or you do not have permission to delete it.", category="danger")
+            return redirect(url_for("dashboard"))
+    else:
+        form = ArticleForm(request.form)
+        if form.validate():
+            updated_title = form.title.data
+            updated_content = form.content.data
+            updated_thumbnail = form.thumbnail.data
+            update_query = "Update articles Set title = %s, content = %s, post_thumbnail = %s where id = %s"
+            cursor = mysql.connection.cursor()
+            cursor.execute(update_query, (updated_title, updated_content, updated_thumbnail, id))
+            mysql.connection.commit()
+            flash(message="The article successfuly updated", category="success")
+            return redirect(url_for("dashboard"))
+        else:
+            flash(message="Something went wrong.", category="danger")
+            return redirect(url_for("dashboard"))
+
+    
+
+# login register logout
 
 @app.route('/register', methods = ["GET", "POST"])
 def register():
